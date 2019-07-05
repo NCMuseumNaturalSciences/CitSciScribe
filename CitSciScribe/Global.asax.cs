96dd8cc54e936c14a9262b895c6af0ca9337e7d9
ben-norton <ben.norton@naturalsciences.org>
2019-07-05 16:22:23 -0400

    Bootstrap fix

--- CitSciScribe/Global.asax.cs
+++ CitSciScribe/Global.asax.cs
@@ -14,7 +14,6 @@ namespace CitSciScribe
             //System.Data.Entity.Database.SetInitializer<DBContext>(null);
             GlobalFilters.Filters.Add(new UserProfileActionFilter(), 0);
             //AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
